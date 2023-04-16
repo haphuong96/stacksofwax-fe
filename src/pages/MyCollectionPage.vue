e55645a710ca7a11ebed8354ba81768fd32aa69e
40381633 <tnguyen09@qub.ac.uk>
2023-04-16 07:52:57 +0700

    Implement My Collections page and pagination

--- src/pages/MyCollectionPage.vue
+++ src/pages/MyCollectionPage.vue
@@ -9,34 +9,58 @@ import { axiosIntance } from "../services/base.service";
 
 const username = localStorage.getItem(localStorageKeys.USERNAME);
 const userId = localStorage.getItem(localStorageKeys.USER_ID);
+
+const defaultPage = 1;
+const defaultPageSize = 10;
+
 const myCollection = ref();
+const total = ref();
 
 onMounted(async () => {
-  const filter = await axiosIntance.get('my-collections');
-  myCollection.value = filter.data;
-  console.log(myCollection.value);
+    fetchMyCollections();
 });
 
 function goToCollections() {
     router.push({ name: routeNames.COLLECTION });
 }
 
-async function goToDraftCollection() {
+function goToDraftCollections(collectionId) {
+
+    router.push({ name: routeNames.DRAFT_COLLECTION_DETAIL, params: { id: collectionId } });
+}
+
+async function createCollection() {
     const res = await axiosIntance.post('collections', {
         user_id: userId
     });
 
     const collectionId = res.data[0].collection_id;
 
-    router.push({name: routeNames.DRAFT_COLLECTION_DETAIL, params: { id: collectionId }});
+    goToDraftCollections(collectionId);
+
+}
+
+async function fetchMyCollections(page, pageSize) {
+    const limit = pageSize || defaultPageSize;
+    const offset = (page - 1) * pageSize || 0;
+    const res = await axiosIntance.get('my-collections', {
+      params: {
+        limit,
+        offset
+      }
+    });;
+
+    myCollection.value = res.data.collections;
+    total.value = res.data.total;
 }
 </script>
 
 <template>
     <a-row class="m-16 p-16">
-        <h3>Want to explore other collections from the Stacks of Wax Community? Check out <a @click="goToCollections">Recent
-                List</a>!</h3>
         <a-col :span="24">
+            <h3>Want to explore other collections from the Stacks of Wax Community? Check out <a
+                    @click="goToCollections">Recent
+                    List</a>!</h3>
             <a-tabs v-model:activeKey="activeKey">
                 <a-tab-pane key="1">
                     <template #tab>
@@ -44,13 +68,23 @@ async function goToDraftCollection() {
                             My Collections
                         </span>
                     </template>
-                    <h4>Collection by {{ username }}</h4>
-                    <a-button @click="goToDraftCollection">Create a new collection</a-button>
-                    <a-list size="small" bordered   v-if="myCollection" :data-source="myCollection">
-                        <template #renderItem="{ item }">
-                            <a-list-item>{{ item }}</a-list-item>
-                        </template>
-                    </a-list>
+                    <a-row>
+                        <a-col :span="24">
+                            <h4>Collection by {{ username }}</h4>
+                            <a-button @click="createCollection">Create a new collection</a-button>
+                            <a-list size="small" bordered v-if="myCollection" :data-source="myCollection">
+                                <template #renderItem="{ item }">
+                                    <a-list-item>
+                                        <a-button type="link" @click="() => goToDraftCollections(item.collection_id)">{{
+                                            item.collection_name }}
+                                        </a-button></a-list-item>
+                                </template>
+                            </a-list>
+
+                            <a-pagination v-model:current="current" :total="total" show-less-items
+                                @change="(page, pageSize) => fetchMyCollections(page, pageSize)" />
+                        </a-col>
+                    </a-row>
                 </a-tab-pane>
                 <a-tab-pane key="2">
                     <template #tab>
