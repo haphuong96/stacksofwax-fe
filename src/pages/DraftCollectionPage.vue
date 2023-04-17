eb232f86118e2a5621f8a43c8a49f42da57a1897
40381633 <tnguyen09@qub.ac.uk>
2023-04-18 06:09:11 +0700

    Implement search albums on collection draft page

--- src/pages/DraftCollectionPage.vue
+++ src/pages/DraftCollectionPage.vue
@@ -6,7 +6,7 @@ import router from "../router";
 import { routeNames } from "../router/route-names";
 import { localStorageKeys } from "../common/local-storage-keys";
 import { axiosIntance } from "../services/base.service";
-import { EditOutlined, EditFilled } from '@ant-design/icons-vue';
+import { EditOutlined, EditFilled, CloseOutlined } from '@ant-design/icons-vue';
 
 const collectionId = router.currentRoute.value.params.id.split("-")[0];
 
@@ -17,11 +17,19 @@ const albumsData = ref();
 const collectionData = ref();
 const createdByData = ref();
 
+const searchAlbumData = ref([]);
+const searchAlbumKeyword = ref();
+
 const isEditMode = ref(false);
 const isDescDisplay = ref(false);
+const isSearchAlbumMode = ref(false);
 
 const collectionName = ref();
 const collectionDesc = ref();
+
+const showHideAlbumSearchSection = () => {
+    isSearchAlbumMode.value = !isSearchAlbumMode.value;
+}
 
 const showHideDescriptionModal = () => {
     isDescDisplay.value = !isDescDisplay.value
@@ -31,11 +39,11 @@ const showEditModal = () => {
 }
 const handleEditOk = () => {
     updateCollection()
-    .then(isEditMode.value = false)
-    .catch((error) => {
-        console.log(error)
-        message.error("Error updating collection")
-    });
+        .then(isEditMode.value = false)
+        .catch((error) => {
+            console.log(error)
+            message.error("Error updating collection")
+        });
 }
 const cancelEdit = () => {
     console.log("cancel")
@@ -71,7 +79,18 @@ async function fetchCollectionById() {
     console.log(createdByData.value)
 }
 
+async function searchAlbum() {
+    const res = await axiosIntance.get(`albums`, {
+        params: {
+            limit: 10,
+            offset: 0,
+            search: searchAlbumKeyword.value
+        }
+    });
 
+    searchAlbumData.value = res.data.albums;
+    console.log(searchAlbumData.value)
+}
 
 </script>
 
@@ -83,7 +102,8 @@ async function fetchCollectionById() {
                     <a-image :width="200" src="https://static-cse.canva.com/blob/1035320/1600w-fxYFTKLArdY.jpg" />
                     <a-button type="link" @click="showHideDescriptionModal">Click here to show description</a-button>
                     <div>
-                        <a-modal v-model:visible="isDescDisplay" :title="`${collectionData.collection_name} - Description`" @ok="showHideDescriptionModal" :maskClosable="false">
+                        <a-modal v-model:visible="isDescDisplay" :title="`${collectionData.collection_name} - Description`"
+                            @ok="showHideDescriptionModal" :maskClosable="false">
                             {{ collectionData.collection_desc }}
                         </a-modal>
                     </div>
@@ -91,22 +111,39 @@ async function fetchCollectionById() {
                 <a-col>
                     <div>Collection <a @click="showEditModal"><edit-filled /></a></div>
                     <div>
-                        <!-- <a-button type="primary" @click="showEditModal">Open Modal</a-button> -->
-                        <a-modal v-model:visible="isEditMode" title="Edit Collection" @ok="handleEditOk" @cancel="cancelEdit" :maskClosable="false">
-                            <p><h3>Title:</h3>
+                        <a-modal v-model:visible="isEditMode" title="Edit Collection" @ok="handleEditOk"
+                            @cancel="cancelEdit" :maskClosable="false">
+                            <p>
+                            <h3>Title:</h3>
                             <a-input v-model:value="collectionName" placeholder="Enter collection name" /></p>
-                            <p><h3>Description:</h3>
-                            <a-textarea v-model:value="collectionDesc" placeholder="[Optional] Write some description for this collection..." :rows="4" /></p>  
+                            <p>
+                            <h3>Description:</h3>
+                            <a-textarea v-model:value="collectionDesc"
+                                placeholder="[Optional] Write some description for this collection..." :rows="4" /></p>
                         </a-modal>
                     </div>
-                    <!-- <h1><a-input v-model:value="collectionData.collection_name" placeholder="Enter collection name" /></h1> -->
                     <h1>{{ collectionName }}</h1>
                     <div>By {{ createdByData.username }}</div>
                 </a-col>
             </a-row>
             <a-row>
                 <a-col :span="24">
-                    
+                    <h1>Album List</h1>
+                    <a-list bordered :data-source="albumsData">
+                        <template #renderItem="{ item }">
+                            <a-list-item>{{ item.album_title }}</a-list-item>
+                        </template>
+                    </a-list>
+                    <a-button type="link" v-if="!isSearchAlbumMode" @click="showHideAlbumSearchSection">Find album</a-button>
+                    <div v-if="isSearchAlbumMode">
+                        <a-input-search v-model:value="searchAlbumKeyword" placeholder="Search album" style="width: 200px"
+                            @search="searchAlbum" /> <a @click="showHideAlbumSearchSection"><close-outlined /></a>
+                        <a-list bordered :data-source="searchAlbumData"  v-if="searchAlbumData.length > 0" >
+                            <template #renderItem="{ item }">
+                                <a-list-item>{{ item.album_title }}</a-list-item>
+                            </template>
+                        </a-list>
+                    </div>
                 </a-col>
             </a-row>
         </a-col>
