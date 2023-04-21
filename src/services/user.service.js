189abdcb9455ac0ac67a092d778620fba834f21e
Phedra <phuongha96@gmail.com>
2023-04-22 00:39:54 +0700

    implement pagination for user info page

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,12 +1,15 @@
+import pagination from "../utils/pagination.helper";
 import { axiosIntance } from "./base.service";
 
-async function fetchUserInfo(userId, limit = 10, offset = 0) {
+async function fetchUserInfo(userId, page = 1, pageSize = 10) {
   try {
+    const { limit, offset } = pagination(page, pageSize);
     const rs = await axiosIntance.get(`/users/${userId}`, {
       params: { limit, offset }
     });
     return rs?.data;
   } catch (error) {
+    debugger;
     return undefined;
   }
 }
