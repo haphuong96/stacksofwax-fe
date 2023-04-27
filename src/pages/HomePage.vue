00a36d3e6367f480f554188a56ee1080bce7403a
40381633 <tnguyen09@qub.ac.uk>
2023-04-28 00:01:23 +0700

    Add homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,24 @@
 <script setup>
+import { navigationService } from '../services/navigation.service';
+
 
 </script>
 
 <template>
-  <div>This is Home Page</div>
+  <a-row class="m-16 d-flex">
+    <a-col :span="16">
+      <img class="homepage-cover" src="https://cdn.dribbble.com/userupload/4102239/file/original-4e946c2cbb727dc31b9b72a8f04c9175.jpg?compress=1&resize=1600x1200"/>
+    </a-col>
+    <a-col :span="8" class="d-flex v-flex justify-center">
+      <h1>Stacks of Wax is a place to share and explore music.</h1>
+      <a class="py-8" @click="navigationService.goToExploreAlbum()">Explore music >>></a>
+      <a @click="navigationService.goToCollections()">Explore music collections from Stack of Wax community >>></a>
+    </a-col>
+  </a-row>
 </template>
 
-<style scoped></style>
+<style scoped>
+.homepage-cover {
+  width: 100%;
+}
+</style>
